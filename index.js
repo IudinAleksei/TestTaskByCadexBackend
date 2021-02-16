d94923e20982a76df1195597c6ce51ca63619d8d
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-16 16:32:39 +0300

    feat: add db file

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const parseSizesFromGet = require('./parseSize');
 const checkRequestSizes = require('./check');
 const USERS_DATA = require('./user-and-sevices');
 
-
 const PORT = process.env.PORT || 3020;
+
+const fileDB = 'db.json';
 
 const app = express();
 
@@ -31,40 +32,50 @@ app.get('/', (req, res) => {
 
 //ng-user-forms endpoints
 
+const readJson = () => {
+  const rawdata = fs.readFileSync(fileDB);
+  const data = JSON.parse(rawdata);
+
+  return data;
+}
+
+const writeJson = (fullFileBody) => {
+  const data = JSON.stringify(fullFileBody, undefined, 2);
+  fs.writeFileSync('student-2.json', data);
+}
+
 app.use(express.json());
 
 app.route('/forms/:field')
   .get((req, res) => {
-      // console.log('base: ', req.params);
+      const responseData = readJson()[req.params.field];
 
-      res.status(200).json(USERS_DATA[req.params.field]);
+      res.status(200).json(responseData);
   })
   .post((req, res) => {
     // console.log('base: ', req.body);
+    // writeJson(req.body);
 
-    // let rawdata = fs.readFileSync('users-and-services.json');
-    // let student = JSON.parse(rawdata);
-    // console.log(student);
-
-    let data = JSON.stringify(req.body, undefined, 2);
-    fs.writeFileSync('student-2.json', data);
     res.status(200).json(req.body);
 })
 
 app.route('/forms/:field/:id')
   .get((req, res) => {
-      // console.log('spec:', req.params);
-      const data = USERS_DATA[req.params.field][req.params.id-1];
-      if (data) {
-        res.status(200).json(USERS_DATA[req.params.field][req.params.id-1]);
+      console.log('spec:', req.params);
+      const responseData = readJson()[req.params.field][req.params.id-1];
+      if (responseData) {
+        res.status(200).json(responseData);
       } else {
-        res.status(404).send();
+        res.status(404).send({message: 'Not found'});
       }
 
 
   })
-  .post((req, res) => {
-    // console.log('spec:', req.body);
+  .patch((req, res) => {
+    const input = readJson();
+    input[req.params.field][req.params.id-1] = req.body;
+    writeJson(input);
+    // console.log('spec patch:', req.params, req.body);
     res.status(200).json(req.body);
 })
 
